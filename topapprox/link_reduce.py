caefaababc4632de95f4cc5edfbc00198a2701bc
shizuo-kaji <shizuo.kaji@gmail.com>
2024-04-14 15:59:51 +0900

    improved link_reduce

--- topapprox/link_reduce.py
+++ topapprox/link_reduce.py
@@ -26,35 +26,32 @@ def _link_reduce(birth, edges, epsilon, keep_basin=False):
     children = dict()
     modified = birth.copy()
     for i in range(edges.shape[0]):
-      ui,vi = edges[i,0],edges[i,1]
-      # # parents of u and v
-      up = parent[ui]
-      vp = parent[vi]
-      death = max(birth[ui],birth[vi])
-      if up != vp: # if their bosses are different (i.e., they belong to different families)
-        if birth[up] < birth[vp]:
-          up,vp = vp,up   # up is the younger and to be killed
-        if birth[up] < death: # a cycle is produced
-            persistence.append((birth[up],death,up))
+        ui,vi = edges[i,0],edges[i,1]
+        # # parents of u and v
+        up = parent[ui]
+        vp = parent[vi]
+        death = max(birth[ui],birth[vi])
+        if up != vp: # if their bosses are different (i.e., they belong to different families)
+            if birth[up] < birth[vp]:
+                up,vp = vp,up   # up is the younger and to be killed
+            # the basin of the younger family will be merged into older's
             if up not in list(children.keys()):
-                children[up] = np.array([up])
-            val=death
-        else:
-            val=np.inf
-        # the basin of the younger family will be merged into older's
-        if up not in list(children.keys()):
-            region = np.array([up])
-        else:
-            region = children[up].copy()
-            if not keep_basin:
-                del children[up]
-        if abs(birth[up]-val)<epsilon:
-            modified[region]=val
-        parent[region] = vp
-        if vp in list(children.keys()):
-          children[vp] = np.append(children[vp], region) # can we skip creating a new array?
-        else:
-          children[vp] = np.append([vp], region)
+                region = np.array([up])
+            else:
+                region = children[up].copy()
+                if not keep_basin:
+                    del children[up]
+            if birth[up] < death: # a cycle is produced
+                persistence.append((birth[up],death,up))
+                if keep_basin and (up not in list(children.keys())):
+                    children[up] = np.array([up])
+                if abs(birth[up]-death)<epsilon:
+                    modified[region]=death
+            parent[region] = vp
+            if vp in list(children.keys()):
+                children[vp] = np.append(children[vp], region) # can we skip creating a new array?
+            else:
+                children[vp] = np.append([vp], region)
     return(modified,persistence,children)
 
 
